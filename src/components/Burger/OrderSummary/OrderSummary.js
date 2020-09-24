75e02d2fe0b3a14efdf067b6af1741b272974412
manuzcheruz <kipkemoi.emanuel@gmail.com>
2020-09-24 06:27:07 +0300

    fully working optimized burger builder

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-        return (<li key={igKey}>
-            <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>);
+class OrderSummary extends Component {
+    componentWillUpdate () {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+            return (<li key={igKey}>
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>);
         });
-    return (
-        <Aux>
+        return (
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>totla price: {props.price.toFixed(2)}</strong></p>
+            <p><strong>totla price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue with checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaceCancel}>CANCEL</Button>
-            <Button btnType="Danger" clicked={props.purchaceContinue}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaceCancel}>CANCEL</Button>
+            <Button btnType="Danger" clicked={this.props.purchaceContinue}>CONTINUE</Button>
         </Aux>
-    );
+        );
+    } 
 };
 
-export default orderSummary;
+export default OrderSummary;
